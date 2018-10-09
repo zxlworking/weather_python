b28c70e1e427b816b303206fefec73275c9c7251
zxlworking <szzhangxiaolong@163.com>
2018-10-09 18:31:24 +0800

    add zhongg guo tianqi wang today weather

--- test6.py
+++ test6.py
@@ -3,7 +3,10 @@
 import json
 import os
 import sys
+from subprocess import Popen, PIPE
+
 from com_zxl_common.ParserUtil import *
+
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -13,14 +16,27 @@ result = {}
 if __name__ == "__main__":
 
     try:
-        package_content = os.popen("aapt d badging %s" % "C:\\zxl\programe\\apache-tomcat-8.5.31\\webapps\\cgi_server\\test.apk").read().decode("utf-8")
+        p = Popen("aapt d badging %s" % "C:\\zxl\programe\\apache-tomcat-8.5.31\\webapps\\cgi_server\\test.apk", shell=True, stdout=PIPE, stderr=PIPE)
+        # fo = open("foo.txt", "a+")
+        # fo.write("--->test6.py--->success--->%s" % p.stdout.read().decode("utf-8"))
+        # fo.write("\n")
+        # fo.close()
+        package_content = p.stdout.read().decode("utf-8")
+
+        # package_content = os.popen("aapt d badging %s" % "C:\\zxl\programe\\apache-tomcat-8.5.31\\webapps\\cgi_server\\test.apk").read().decode("utf-8")
         # print package_content
         mParserUtil = ParserUtil()
         mParserUtil.parse_today_apk_package_info(package_content, result)
+
     except BaseException, e:
         print e
         result["code"] = -1
         result["desc"] = "获取版本信息失败"
+
+        fo = open("foo.txt", "a+")
+        fo.write("--->test6.py--->fail")
+        fo.write("\n")
+        fo.close()
 
 
     print "Content-type:text/html;charset=UTF-8"
